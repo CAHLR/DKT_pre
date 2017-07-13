350fa8c5b34348ca893d9b2bc420dcec2e7735e0
KevinHuuu <hcr14@mails.tsinghua.edu.cn>
2017-07-13 16:14:52 -0700

    fixed bugs

--- eye_preprocessing.py
+++ eye_preprocessing.py
@@ -4,23 +4,24 @@ import numpy as np
 import pandas as pd
 import pdb
 import os
-
+import pdb
 root = '../../'
-writer = csv.writer(open('../../DKT_atoms.csv','w',newline=''))
+writer = csv.writer(open(root + 'DKT_atoms.csv','w',newline=''))
 imputed_list = []
 for dirpath,dirnames,filenames in os.walk(root):
     for filename in filenames:
         if filename.endswith('_imputed.csv'):
-            imputed_list.append(filename)
+            imputed_list.append(dirpath + '/' + filename)
+
 
 problem_ID = 0 # make each question to be an ID
 problem_set = {} # unique problem type
 answer_set = {'CORRECT':1, 'INCORRECT':0}
-print ('The num of students is ', len(filename))
+print ('The num of students is ', len(imputed_list))
 
 for imputed_file in imputed_list:
 
-    student = pd.read_csv(root + imputed_file, low_memory=False)
+    student = pd.read_csv(imputed_file, low_memory=False)
     Response_null = pd.isnull(student["Response"])
     student_res = student[['question','Response']][Response_null == False]
     student_res = student_res[student_res['question'] != '_root'][student_res['question'] != 'done'][student_res['question'] != 'nl'][student_res['Response'] != 'HINT']
@@ -35,7 +36,7 @@ for imputed_file in imputed_list:
         problem_IDs.append(problem_set[row['question']])
         answers.append(answer_set[row['Response']])
     num_problem = len(problem_IDs)
-    print ('num_students',len(filename),'\n num_problem',num_problem,'\n len(problem_IDs) ',len(problem_IDs),'\n len(answers)', len(answers))
+    print ('num_students',len(imputed_list),'\n num_problem',num_problem,'\n len(problem_IDs) ',len(problem_IDs),'\n len(answers)', len(answers))
 
     writer.writerow([num_problem])
     writer.writerow(problem_IDs)
