e8a0ac1ef6c36b92ee1c3d9518825bf2b076ccd7
KevinHuuu <hcr14@mails.tsinghua.edu.cn>
2017-07-16 10:29:17 -0700

    from now we work at the eye-tracking repo by div

--- eye_preprocessing.py
+++ eye_preprocessing.py
@@ -17,30 +17,43 @@ for dirpath,dirnames,filenames in os.walk(root):
 problem_ID = 0 # make each question to be an ID
 problem_set = {} # unique problem type
 answer_set = {'CORRECT':1, 'INCORRECT':0}
-print ('The num of students is ', len(imputed_list))
-
+num_student = len(imputed_list)
+print ('The num of students is ', num_student)
 for imputed_file in imputed_list:
 
     student = pd.read_csv(imputed_file, low_memory=False)
     Response_null = pd.isnull(student["Response"])
-    student_res = student[['question','Response']][Response_null == False]
+    student_res = student[['Screen_Number','Representation_Number_Within_Screen','question','Response']][Response_null == False]
     student_res = student_res[student_res['question'] != '_root'][student_res['question'] != 'done'][student_res['question'] != 'nl'][student_res['Response'] != 'HINT']
     problem_IDs = [] # the total problems sequence of each student.
     answers = [] # the total answers sequence of each student
-
+    filter_student = False
+    if len(student['Screen_Number'].unique()) == 1:
+        filter_student = True
+        
     for ix, row in student_res.iterrows():
-        # print (row['Response'],row['question'])
+        if row['Screen_Number']>3 :
+            break
+        if str(row['Representation_Number_Within_Screen'])== 'nan':
+            filter_student = True
+            break
+        SPquestion = str(row['Screen_Number']) + str(row['Representation_Number_Within_Screen']) + row['question']
+        # print (SPquestion)
         if not row['question'] in problem_set:
             problem_ID += 1
             problem_set.update({row['question']: problem_ID})
         problem_IDs.append(problem_set[row['question']])
         answers.append(answer_set[row['Response']])
     num_problem = len(problem_IDs)
-    print ('num_students',len(imputed_list),'\n num_problem',num_problem,'\n len(problem_IDs) ',len(problem_IDs),'\n len(answers)', len(answers))
-
+    if filter_student == True: # Screen or page number missing in this student so we filter it out
+        print (imputed_file,'filtering out, screen/page info missing')
+        num_student -= 1
+        continue
     writer.writerow([num_problem])
     writer.writerow(problem_IDs)
     writer.writerow(answers)
+    print ('num_problem',num_problem,'\n len(problem_IDs) ',len(problem_IDs),'\n len(answers)', len(answers))
+print ('num_students',num_student)
 
 
 
